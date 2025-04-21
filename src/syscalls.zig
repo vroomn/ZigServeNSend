2edfa59f9d90bc491f276271d1f8baa495f1003b
vroom <harrison.hilpert@gmail.com>
2025-04-20 22:35:27 -0600

    Easy calling of Linux syscalls

---
+++ src/syscalls.zig
@@ -0,0 +1,68 @@
+pub inline fn syscallOne(number: usize, arg1: anytype) usize {
+    return asm volatile ("syscall"
+        : [ret] "={rax}" (-> usize),
+        : [number] "{rax}" (number),
+          [arg1] "{rdi}" (arg1),
+        : "rcx", "r11"
+    );
+}
+
+pub inline fn syscallTwo(number: usize, arg1: anytype, arg2: anytype) usize {
+    return asm volatile ("syscall"
+        : [ret] "={rax}" (-> usize),
+        : [number] "{rax}" (number),
+          [arg1] "{rdi}" (arg1),
+          [arg2] "{rsi}" (arg2),
+        : "rcx", "r11"
+    );
+}
+
+pub inline fn syscallThree(number: usize, arg1: anytype, arg2: anytype, arg3: anytype) usize {
+    return asm volatile ("syscall"
+        : [ret] "={rax}" (-> usize),
+        : [number] "{rax}" (number),
+          [arg1] "{rdi}" (arg1),
+          [arg2] "{rsi}" (arg2),
+          [arg3] "{rdx}" (arg3),
+        : "rcx", "r11"
+    );
+}
+
+pub inline fn syscallFour(number: usize, arg1: anytype, arg2: anytype, arg3: anytype, arg4: anytype) usize {
+    return asm volatile ("syscall"
+        : [ret] "={rax}" (-> usize),
+        : [number] "{rax}" (number),
+          [arg1] "{rdi}" (arg1),
+          [arg2] "{rsi}" (arg2),
+          [arg3] "{rdx}" (arg3),
+          [arg4] "{r10}" (arg4),
+        : "rcx", "r11"
+    );
+}
+
+pub inline fn syscallFive(number: usize, arg1: anytype, arg2: anytype, arg3: anytype, arg4: anytype, arg5: anytype) usize {
+    return asm volatile ("syscall"
+        : [ret] "={rax}" (-> usize),
+        : [number] "{rax}" (number),
+          [arg1] "{rdi}" (arg1),
+          [arg2] "{rsi}" (arg2),
+          [arg3] "{rdx}" (arg3),
+          [arg4] "{r10}" (arg4),
+          [arg5] "{r8}" (arg5),
+        : "rcx", "r11"
+    );
+}
+
+pub inline fn syscallSix(number: usize, arg1: anytype, arg2: anytype, arg3: anytype, arg4: anytype, arg5: anytype, arg6: anytype) usize {
+    return asm volatile ("syscall"
+        : [ret] "={rax}" (-> usize),
+        : [number] "{rax}" (number),
+          [arg1] "{rdi}" (arg1),
+          [arg2] "{rsi}" (arg2),
+          [arg3] "{rdx}" (arg3),
+          [arg4] "{r10}" (arg4),
+          [arg5] "{r8}" (arg5),
+          [arg6] "{r9}" (arg6),
+        : "rcx", "r11"
+    );
+}
